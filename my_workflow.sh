282002663e1689e52d18498294256f16bc3e4474
Dimitris Dafnoudis <dimitrisdaf@hotmail.com>
2025-03-29 13:21:38 +0100

    my workflow

--- my_workflow.sh
+++ my_workflow.sh
@@ -1,44 +1,33 @@
 #!/bin/bash
 
-# Define the variables
-read1=$1
-read2=$2
-outdir=$3
+# Find the first file that matches the pattern *_1.fastq.gz*
+read1=$(ls Dataset/*_1.fastq.gz* 2>/dev/null | head -n 1)
 
-echo "Input read files: $read1, $read2"
-echo "Output directory: $outdir"
+# Find the first file that matches the pattern *_2.fastq.gz*
+read2=$(ls Dataset/*_2.fastq.gz* 2>/dev/null | head -n 1)
+
+# Define output directories
+outdir="Results"
+outdir_shovill="$outdir/shovill"
+db_dir="databases"
+
 
 # Check if data files exist before downloading
-if [ ! -f "Dataset/ERR14229040_1.fastq.gz" ] || [ ! -f "Dataset/ERR14229040_2.fastq.gz" ]; then
+if [ ! -f "$read1" ] || [ ! -f "$read2" ]; then
   bash my_scripts/download_data.sh
 else
   echo "Data files already exist, skipping download."
 fi
 
+
 # Check if FastQC output exists before running
-if [ ! -f "$outdir/ERR14229040_1_fastqc.html" ]; then
-  bash my_scripts/fastqc.sh $read1 $read2 $outdir
-else
-  echo "FastQC results already exist, skipping FastQC."
-fi
+bash my_scripts/fastqc.sh $read1 $read2 $outdir/FastQC
 
 # Check if Shovill output exists before running
-if [ ! -f "$outdir/shovill/contigs.fa" ]; then
-  bash my_scripts/shovill.sh $read1 $read2 $outdir
-else
-  echo "Shovill output already exists, skipping Shovill."
-fi
+bash my_scripts/shovill.sh $read1 $read2 $outdir_shovill
 
 # Check if database is installed before installing
-if [ ! -d "$db_dir/resfinder_db" ]; then
-  bash my_scripts/install_databases.sh $db_dir
-else
-  echo "Databases already installed, skipping installation."
-fi
+bash my_scripts/install_databases.sh $db_dir
 
 # Check if ResFinder output exists before running
-if [ ! -f "$outdir/ResFinder_output.txt" ]; then
-  bash my_scripts/execute_resfinder.sh $read1 $read2 $outdir $database_dir
-else
-  echo "ResFinder results already exist, skipping ResFinder."
-fi
+bash my_scripts/execute_resfinder.sh $read1 $read2 $outdir $db_dir
